90816a560227721cf5f2131b04e54485fba09f2e
chenjie <chenjie@szy.cn>
2024-08-31 11:55:47 +0800

    feat: 调整发布配置

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue'
 import { createBaseApi, createWxApi } from './api'
 import { h5SDKConfig, type H5SDKConfig } from './config'
-import Button from './components/button/index'
-import Loading from './components/loading/index'
+import Button from './components/button'
+import Loading from './components/loading'
 
 export { version } from '../package.json'
 
@@ -14,8 +14,8 @@ export * from './types'
 export * from './utils'
 export * from './config'
 
-export * from './components/button/index'
-export * from './components/loading/index'
+export * from './components/button'
+export * from './components/loading'
 
 export default (config: H5SDKConfig) => {
   const createApi = () => {
