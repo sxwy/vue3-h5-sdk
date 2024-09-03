07439515a0f8978b3b43951ca3e4dc327f088ee0
chenjie <785801610@qq.com>
2024-09-03 23:08:07 +0800

    feat: 组件优化

--- src/components/button/__tests__/Button.test.ts
+++ src/components/button/__tests__/Button.test.ts
@@ -1,10 +1,9 @@
 import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
-
 import Button from '../Button'
 
 describe('attr', () => {
-  test('prop', () => {
+  test('class', () => {
     const wrapper = mount(Button)
     expect(wrapper.get('.sxwy-button').text()).toBe('button')
   })
--- src/components/loading/__tests__/Loading.test.ts
+++ src/components/loading/__tests__/Loading.test.ts
@@ -1,10 +1,9 @@
 import { describe, expect, test } from 'vitest'
 import { mount } from '@vue/test-utils'
-
 import Loading from '../Loading'
 
 describe('attr', () => {
-  test('prop', () => {
+  test('class', () => {
     const wrapper = mount(Loading)
     expect(wrapper.classes()).toContain('sxwy-loading')
   })
