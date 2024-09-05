323bf87ca085a1d0e76e0d266b6558f414dcab94
chenjie <785801610@qq.com>
2024-09-05 23:32:04 +0800

    feat: vitepress 初体验

---
+++ docs/Basic.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { Button } from '../src'
+</script>
+<template>
+  <div>哈哈哈</div>
+  <Button />
+  <!-- <Button> hello </Button>
+  <Button type="primary"> Primary </Button>
+  <Button type="danger"> Danger </Button>
+  <Button loading> Loading </Button> -->
+</template>
---
+++ docs/demo/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <Button />
+</template>
+
+<script setup>
+  import { Button } from '@sxwy/h5-sdk'
+</script>
